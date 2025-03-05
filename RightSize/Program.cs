3438de35b0200705698afe2ddc414b2cec5607b5
Simão Durão <simaodurao@gmail.com>
2025-03-05 14:24:23 +0000

    Added RightSize to the Sln and Verified output

---
+++ RightSize/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RightSize
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            foreach (string arg in args)
+            {
+                if (arg.Length>=8)
+                {
+                    Console.WriteLine("[EARLY STOP]");
+                    break;
+                }
+                if (arg.Length>3)
+                {
+                    Console.WriteLine(arg);
+                }
+            }
+        }
+    }
+}
