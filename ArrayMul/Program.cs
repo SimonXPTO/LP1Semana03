1bbae446a0532e7bb0a5d3d76e053b58d715d334
Simão Durão <simaodurao@gmail.com>
2025-03-05 15:02:12 +0000

    created Matrix A and B

--- ArrayMul/Program.cs
+++ ArrayMul/Program.cs
@@ -6,7 +6,18 @@ namespace ArrayMul
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            int [,] matrixA = new int [1, 1];
+            int [,] matrixB = new int [1, 0];
+
+            float arg1= float.Parse(args[0]);
+            float arg2= float.Parse(args[1]);
+            float arg3= float.Parse(args[2]);
+            float arg4= float.Parse(args[3]);
+            float arg5= float.Parse(args[4]);
+            float arg6= float.Parse(args[5]);
+
+            matrixA[0, 0] = 0;
+
         }
     }
 }
