27c03bcb53326db4aa2f82523db56466333738d1
Simão Durão <simaodurao@gmail.com>
2025-03-06 13:48:11 +0000

    Added and called 2x Pow2Until

--- Pow2Until/Program.cs
+++ Pow2Until/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Net.NetworkInformation;
 
 namespace Pow2Until
 {
     public class Program
     {
+        private static void PowerOf2Until5()
+        {
+            for (int i = 1; i<=(1 <<5); i = i<<1)
+            {
+                Console.WriteLine(i);
+            }
+        }
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            PowerOf2Until5();
+            PowerOf2Until5();
+            
         }
     }
 }
